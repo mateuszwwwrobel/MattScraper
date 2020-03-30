20f2d42217eec1a12dda6a5886d3a4da9b06a4c5
Mateusz Wrobel <mateusz.wwwrobel@gmail.com>
2020-03-30 18:15:11 +0100

    add ebay search functionality

--- my_app/views.py
+++ my_app/views.py
@@ -1,11 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 from django.shortcuts import render
-from requests.compat import quote_plus
 from . import models
 
-BASE_CRAIGSLIST_URL = 'https://aberdeen.craigslist.org/search/sss?query={}&sort=rel'
-BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
+BASE_CRAIGSLIST_URL = 'https://www.ebay.co.uk/sch/i.html?_from=R40&_nkw={}&_sacat=0&LH_BIN=1&_sop=10'
+BASE_IMAGE_URL = 'https://www.globalinnovationforum.com/wp-content/uploads/2014/03/ebay-logo.jpg'
 
 
 def home(request):
@@ -15,30 +14,24 @@ def home(request):
 def new_search(request):
     search = request.POST.get('search')
     models.Search.objects.create(search=search)
-    final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))
+    final_url = BASE_CRAIGSLIST_URL.format((search))
     response = requests.get(final_url)
     data = response.text
-    soup = BeautifulSoup(data, features='html.parser')
+    soup = BeautifulSoup(data, features='lxml')
 
-    post_listings = soup.find_all('li', {'class': 'result-row'})
+#    post_listings = soup.find_all('li.lvprice', 'h3.lvtitle',)
 
     final_postings = []
 
-    for post in post_listings:
-        post_title = post.find(class_='result-title').text
-        post_url = post.find('a').get('href')
+    for titles in soup.select("h3.lvtitle"):
+        post_title = titles.find('a')['title'].split('access ')[1]
+        post_url = titles.find('a').get('href')
+        post_image_url = BASE_IMAGE_URL
 
-        if post.find(class_='result-price'):
-            post_price = post.find(class_='result-price').text
-        else:
-            post_price = 'N/A'
+        for prices in soup.select('li.lvprice'):
+            post_price = prices.find('span').text
 
-        if post.find(class_='result-image').get('data-ids'):
-            post_image_id = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
-            post_image_url = BASE_IMAGE_URL.format(post_image_id)
-            print(post_image_url)
-        else:
-            post_image_url = 'https://craigslist.org/images/peace.jpg'
+
 
         final_postings.append((post_title, post_url, post_price, post_image_url))
 
